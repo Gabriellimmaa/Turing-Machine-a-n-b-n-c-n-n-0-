db7e77faacd1ff3afbe166cfa861a73b756e37bc
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2021-11-24 15:05:01 -0300

    Update tm.py

--- tm.py
+++ tm.py
@@ -1,71 +1,66 @@
 import os
 from colorama import Fore, init
 
-def instrucoes():
-    machine_instructions = {('q0', 'a'): ('q1', 'X', '>'), ('q1', 'a'): ('q1', 'a', '>'), ('q1', 'Y'): ('q1', 'Y', '>'), ('q1', 'b'): ('q2', 'Y', '>'), ('q2', 'b'): ('q2', 'b', '>'), ('q2', 'Z'): ('q2', 'Z', '>'), ('q2', 'c'): ('q3', 'Z', '<'), ('q3', 'Z'): ('q3', 'Z', '<'), ('q3', 'b'): ('q3', 'b', '<'), ('q3', 'Y'): ('q3', 'Y', '<'), ('q3', 'a'): ('q3', 'a', '<'), ('q3', 'X'): ('q0', 'X', '>'), ('q0', 'Y'): ('q4', 'Y', '>'), ('q4', 'Y'): ('q4', 'Y', '>'), ('q4', 'Z'): ('q4', 'Z', '>'), ('q4', '_'): ('q5', '_', '<'), ('q5', '_'): ('q5', '_', '-')}
-    main('q0', 'q5', machine_instructions)
-
 def aux(inpt):
-    tape = list(inpt)
-    tape.append("_")
-    return tape
+    fita = list(inpt)
+    fita.append("_")
+    return fita
 
-def main(ini_state, accepting, machine_instructions):
+def main():
     while True:
         os.system('cls')
         usr_inpt = input(f"\n{Fore.YELLOW}Digite um string teste:{Fore.RESET} ")
-        tape = aux(usr_inpt)
-        machine(ini_state, accepting, tape, machine_instructions)
+        fita = aux(usr_inpt)
+        instrucoes = {('q0', 'a'): ('q1', 'X', '>'), ('q1', 'a'): ('q1', 'a', '>'), ('q1', 'Y'): ('q1', 'Y', '>'),
+                      ('q1', 'b'): ('q2', 'Y', '>'), ('q2', 'b'): ('q2', 'b', '>'), ('q2', 'Z'): ('q2', 'Z', '>'),
+                      ('q2', 'c'): ('q3', 'Z', '<'), ('q3', 'Z'): ('q3', 'Z', '<'), ('q3', 'b'): ('q3', 'b', '<'),
+                      ('q3', 'Y'): ('q3', 'Y', '<'), ('q3', 'a'): ('q3', 'a', '<'), ('q3', 'X'): ('q0', 'X', '>'),
+                      ('q0', 'Y'): ('q4', 'Y', '>'), ('q4', 'Y'): ('q4', 'Y', '>'), ('q4', 'Z'): ('q4', 'Z', '>'),
+                      ('q4', '_'): ('q5', '_', '<'), ('q5', '_'): ('q5', '_', '-')}
+        machine('q0', 'q5', fita, instrucoes)
         input("\nPressione ENTER para continuar")
 
-def machine(ini_state, accepting, tape, machine_instructions):
-    machine = machine_instructions
-
-    reject = False
-    current_state = ini_state
-    accepting_state = accepting
+def machine(_estadoInicial, _estadoFinal, fita, instrucoes):
+    machine = instrucoes
+    resultado = False
+    estado = _estadoInicial
+    estadoAceito = _estadoFinal
     head = 0
-    tape_string = ''.join(tape)
 
     print('')
-    print(tape_string)
+    print(''.join(fita))
 
-    while current_state != accepting_state:
-
-        slot = tape[head]
+    while estado != estadoAceito:
+        slot = fita[head]
         print(' ' * (head) + f'{Fore.YELLOW}^{Fore.RESET}')
         for key, value in machine.items():
-            machine_states = key[0]
-            read_symbol = key[1]
+            estadoMaquina = key[0]
+            simbolo = key[1]
 
-            if (current_state, slot) not in machine:
+            if (estado, slot) not in machine:
                 reject = True
-                current_state = accepting_state
+                estado = estadoAceito
                 break
 
             else:
-                if machine_states == current_state and read_symbol == slot:
-                    instuction = value
-                    current_state = instuction[0]
-                    write_symbol = instuction[1]
-                    tape[head] = write_symbol
-                    tape_string = ''.join(tape)
-                    print(tape_string)
-                    direction = instuction[2]
+                if estadoMaquina == estado and simbolo == slot:
+                    estado = value[0]
+                    fita[head] = value[1]
+                    print(''.join(fita))
 
-                    if direction == ">":
+                    if value[2] == ">":
                         head = head + 1
-                    if direction == "<":
+                    if value[2] == "<":
                         head = head - 1
                     break
 
-    machineEnd(reject)
+    check(resultado)
 
-def machineEnd(reject):
-    if reject == True:
+def check(resultado):
+    if resultado == True:
         print(F"\n{Fore.RED}REJEITOU!{Fore.RESET}")
     else:
         print(f"\n{Fore.GREEN}ACEITOU!{Fore.RESET}")
 
 
-instrucoes()
+main()
